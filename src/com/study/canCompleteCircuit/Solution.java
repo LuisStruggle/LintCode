8ca84fdea921ab3bfc0fc1b11f2f1507e7d644dc
LuisStruggle <18300767078@163.com>
2018-04-09 17:35:00 +0800

    新增算法

--- src/com/study/canCompleteCircuit/Solution.java
+++ src/com/study/canCompleteCircuit/Solution.java
@@ -6,7 +6,7 @@ public class Solution
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-
+        System.out.println(new Solution().canCompleteCircuit(new int[] { 1, 1, 3, 1 }, new int[] { 2, 2, 1, 1 }));
     }
 
     /**
@@ -16,7 +16,8 @@ public class Solution
      */
     public int canCompleteCircuit(int[] gas, int[] cost)
     {
-        // write your code here
+        // 用贪心算法，找到一个满足的，一次查看后面的是否满足，后面不满足，则放弃，重新开始找满足的。当找完一圈任然没有满足的就说明没有满足要求的路径。
+        return 0;
     }
 
 }
